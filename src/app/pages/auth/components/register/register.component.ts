d7401726e515bade3e98350b516be4dac8d49bb2
eldari <xachidze.eldari@gmail.com>
2023-02-01 19:24:40 +0400

    confirmPassword: fix

--- src/app/pages/auth/components/register/register.component.ts
+++ src/app/pages/auth/components/register/register.component.ts
@@ -14,7 +14,7 @@ export class RegisterComponent implements OnInit {
     firstName: new FormControl('', Validators.required),
     lastName: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', Validators.required),
+    password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
     confirmPassword: new FormControl(null, Validators.required),
   }, {validators: this.ConfirmedValidator('password', 'confirmPassword')})
 
